bcc713575260ff0f9d575fb022720f1b59c49a2d
MaximeBernard <85155827+MBernAca@users.noreply.github.com>
2026-06-06 16:19:59 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pecubegui",
+    version="0.1.0",
+    description="A GUI-based tool for the Pecube thermochronology software.",
+    author="Maxime Bernard",
+    url="https://github.com/OpenThermochronology/PecubeGUI",
+    # This identifies the sub-directories containing __init__.py (Utils, Topography, etc.)
+    packages=find_packages(),
+    py_modules=["main"],
+    include_package_data=True,
+    install_requires=[
+        "PyQt5",
+        "numpy",
+        "pandas",
+        "scipy",
+        "matplotlib",
+        "pyvista",
+        "pyvistaqt",
+        "folium",
+        "xarray",
+        "bmi-topography",
+        "cmcrameri",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pecubegui = main:main",
+        ],
+    },
+)
